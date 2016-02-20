8a298d49172094c751692f35a4bc7a1d4914cede
wangchenxicool <wang_chen_xi_cool@qq.com>
2016-02-20 14:11:25 +0800

    change test_log.cpp

--- test_log.cpp
+++ test_log.cpp
@@ -8,39 +8,32 @@
 
 using namespace std;
 
-void* thr_fn (void* arg)
-{
+void* thr_fn (void* arg) {
     int n = 20;
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         DEBUG_LOG ("12345:%d", i);
     }
     return (void*) 0;
 }
 
-void test_mult_thread()
-{
+void test_mult_thread() {
     const int n = 5;
     pthread_t tid[n];
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         pthread_create (&tid[i], NULL, thr_fn, NULL);
     }
 
-    for (int i = 0; i < n; ++i)
-    {
+    for (int i = 0; i < n; ++i) {
         pthread_join (tid[i], NULL);
     }
 }
 
 
-void test_print()
-{
+void test_print() {
     DEBUG_LOG ("int: %d, str: %s, double: %lf", 1, "hello world", 1.5);
 }
 
-int main()
-{
+int main() {
     INIT_LOG ("log");
     test_print();
     test_mult_thread();
